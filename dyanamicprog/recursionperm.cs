65b91ac7d55d887aa78ba2048c7e782e12701780
akr1986 <akr1986@gmail.com>
2021-02-17 23:34:26 -0500

    * alokcodes.sln: Added binary recursion code
    * recursionperm.cs:

---
+++ dyanamicprog/recursionperm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace dyanamicprog
+{
+    public class recursionperm
+    {
+        public void binaryrec(int n, string slate, List<string> lst)
+        {
+            if (n <= 0)
+            {
+                lst.Add(slate);
+                return;
+            }
+            else
+            {
+                binaryrec(n - 1, slate + "0", lst);
+                binaryrec(n - 1, slate + "1", lst);
+            }
+
+        }
+        public void binarynumber(int n)
+        {
+            List<string> lst = new List<string>();
+            binaryrec(n, "", lst);
+            lst.ForEach(Console.WriteLine);
+        }
+    }
+}
